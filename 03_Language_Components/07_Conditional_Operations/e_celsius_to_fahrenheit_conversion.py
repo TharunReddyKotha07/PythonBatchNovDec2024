6c5a89f845b23c5ef87c568e61f993cb432cd930
TharunReddyKotha07 <tharunreddykothaktr@gmail.com>
2024-12-04 02:23:03 +0000

    class10 - assignments

---
+++ 03_Language_Components/07_Conditional_Operations/e_celsius_to_fahrenheit_conversion.py
@@ -0,0 +1,10 @@
+
+"""
+Purpose: Temperature Conversions
+    - celsius to fahrenheit
+"""
+temp_in_c = float(input("Enter a temparature in Celsius: "))
+
+temp_in_f =  (temp_in_c * (9/5)) + 32
+
+print(temp_in_c,"°C = ", temp_in_f,"°F")
